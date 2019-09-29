6e5fc36cb16fdfbdcef5e7daa29be6ec906ac235
Stephen Martin <lockwood@opperline.com>
2019-09-29 09:22:41 -0700

    9.2 is tree symmetric

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,9 +1,19 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
+bool symmetry(const unique_ptr<BinaryTreeNode<int>>& l, const unique_ptr<BinaryTreeNode<int>>& r) {
+  if (!l && !r) return true;
+
+  if (l && r && l->data == r->data)
+    return symmetry(l->left, r->right) && symmetry(l->right, r->left);
+
+  return false;
+}
+
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  if (!tree) return true;
+
+  return symmetry(tree->left, tree->right);
 }
 
 int main(int argc, char* argv[]) {
