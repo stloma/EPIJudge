79a2e3e473231dea09d95d71da35434b4720b565
Stephen Martin <lockwood@opperline.com>
2019-10-02 15:26:13 -0700

    10.3 sort an almost sorted array

--- epi_judge_cpp/sort_almost_sorted_array.cc
+++ epi_judge_cpp/sort_almost_sorted_array.cc
@@ -1,11 +1,32 @@
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::priority_queue;
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end, int k) {
-  // TODO - you fill in here.
-  return {};
+
+  vector<int> A;
+  priority_queue<int> pq;
+  while (sequence_begin != sequence_end)
+    A.push_back(*sequence_begin++);
+
+  for (int i = 0; i < k; ++i) {
+    pq.push(-A[i]);
+  }
+
+  int i = 0;
+  for (; k < A.size(); ++i) {
+    A[i] = -pq.top(); pq.pop();
+    pq.push(-A[k++]);
+  }
+
+  while (!pq.empty()) {
+    A[i++] = -pq.top(); pq.pop();
+  }
+
+  return A;
 }
 vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
                                                int k) {
