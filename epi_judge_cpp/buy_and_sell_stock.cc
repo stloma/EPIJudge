6937d1167bd798a5e41bde197073afb85e9c2ade
Stephen Martin <lockwood@opperline.com>
2019-10-02 05:48:15 -0700

    5.6 buy and sell a stock

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,9 +1,18 @@
 #include <vector>
+#include <cfloat>
 #include "test_framework/generic_test.h"
 using std::vector;
 double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+
+  double min_buy = prices[0];
+  double max_profit = DBL_MIN;
+
+  for (int i = 1; i < prices.size(); ++i) {
+    max_profit = std::max(max_profit, prices[i] - min_buy);
+    min_buy = std::min(min_buy, prices[i]);
+  }
+
+  return max_profit;
 }
 
 int main(int argc, char* argv[]) {
