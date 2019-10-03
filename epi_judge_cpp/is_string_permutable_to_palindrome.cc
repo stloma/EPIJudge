103fa01113cd448e330849bbf36ee7b25c960a36
Stephen Martin <lockwood@opperline.com>
2019-10-03 08:44:55 -0700

    12.1 test for palindromic permutations

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,10 +1,35 @@
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include "test_framework/generic_test.h"
 using std::string;
+using std::unordered_map;
+using std::unordered_set;
 
 bool CanFormPalindrome(const string& s) {
-  // TODO - you fill in here.
-  return true;
+
+  unordered_set<char> odds;
+
+  for (const auto& c : s) {
+    if (odds.find(c) != odds.end())
+      odds.erase(c);
+    else
+      odds.insert(c);
+  }
+
+  return odds.size() <= 1;
+
+  // unordered_map<char,int> freq;
+
+  // for (const auto& c : s)
+  //   freq[c]++;
+
+  // int odds = 0;
+  // for (auto [k,v] : freq) {
+  //   if (v % 2 != 0) odds++;
+  // }
+
+  // return odds <= 1;
 }
 
 int main(int argc, char* argv[]) {
