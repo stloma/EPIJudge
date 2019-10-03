7e8e8eafe6ed323bab00e0e02fce010393d2f4df
Stephen Martin <lockwood@opperline.com>
2019-10-03 08:18:37 -0700

    11.8 kth largest element

--- epi_judge_cpp/kth_largest_in_array.cc
+++ epi_judge_cpp/kth_largest_in_array.cc
@@ -2,11 +2,47 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+int find_idx(vector<int>& A, int l, int r) {
+
+  // Take median of three
+  int right = A[r];
+  int left = A[l];
+  int m = A[(r+l)/ 2];
+
+  auto tmp = vector<int> {right, left, m};
+  std::sort(tmp.begin(), tmp.end());
+  int pivot = tmp[1];
+
+  if (pivot == left) std::swap(A[r], A[l]);
+  else if (pivot == m) std::swap(A[r], A[(r+l)/ 2]);
+
+  int j = l - 1;
+  for (; l < r; ++l) {
+    if (A[l] <= pivot)
+      std::swap(A[++j], A[l]);
+  }
+
+  std::swap(A[++j], A[r]);
+
+  return j;
+}
+
 // The numbering starts from one, i.e., if A = [3, 1, -1, 2] then
 // FindKthLargest(1, A) returns 3, FindKthLargest(2, A) returns 2,
 // FindKthLargest(3, A) returns 1, and FindKthLargest(4, A) returns -1.
 int FindKthLargest(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
+  auto& A = *A_ptr;
+
+  int l = 0, r = A.size()-1, idx = A.size()-k;
+
+  while (l <= r) {
+    auto result = find_idx(A, l, r);
+    if (result == idx) return A[result];
+
+    if (result < idx) l = result + 1;
+    else r = result - 1;
+  }
+
   return 0;
 }
 int FindKthLargestWrapper(int k, vector<int>& A) {
