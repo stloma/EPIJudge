113b6c5c4b3bc2dd6f48e857e3103f1eb9589dae
Stephen Martin <lockwood@opperline.com>
2019-10-04 07:52:27 -0700

    13.1 intersection of two sorted arrays

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -1,11 +1,36 @@
 #include <vector>
+#include <algorithm>
 #include "test_framework/generic_test.h"
 using std::vector;
 
 vector<int> IntersectTwoSortedArrays(const vector<int>& A,
                                      const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+
+  if (A.empty() || B.empty()) return {};
+
+  vector<int> result;
+  int a = 0, b = 0;
+
+  // Average: 9 us, median: 1 us
+  while (a < A.size() && b < B.size()) {
+    if (A[a] == B[b]) {
+      if (result.empty()) result.push_back(A[a]);
+      else if (result.back() != A[a]) result.push_back(A[a]);
+      a++; b++;
+    } else if (A[a] < B[b])
+      a++;
+    else
+      b++;
+  }
+
+  // Average: 47 us, median: 2 us
+  // for (int i = 0; i < A.size(); i++) {
+  //   if (i > 0 && A[i] == A[i-1]) continue;
+  //   if (std::binary_search(B.begin(), B.end(), A[i]))
+  //     result.push_back(A[i]);
+  // }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
