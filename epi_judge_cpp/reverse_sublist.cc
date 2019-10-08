414fbcad8da8a42c9a9ea6a269fa342d7aa3a5e7
Stephen Martin <lockwood@opperline.com>
2019-10-08 09:14:04 -0700

    7.2 reverse sublist

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -3,8 +3,22 @@
 
 shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
                                          int finish) {
-  // TODO - you fill in here.
-  return nullptr;
+
+  auto dummy = make_shared<ListNode<int>> (0);
+  dummy->next = L;
+  auto left = dummy;
+
+  for (int i = 1; i < start; ++i)
+    left = left->next;
+
+  for (auto iter = left->next; start < finish; ++start) {
+    auto tmp = iter->next;
+    iter->next = tmp->next;
+    tmp->next = left->next;
+    left->next = tmp;
+  }
+
+  return dummy->next;
 }
 
 int main(int argc, char* argv[]) {
