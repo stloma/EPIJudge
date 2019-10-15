9567a0751d280228d0c7fba71a4475dd9f98ae63
Stephen Martin <lockwood@opperline.com>
2019-10-15 11:32:10 -0700

    16.2 Levenshtein distance

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -5,8 +5,23 @@ using std::string;
 using std::vector;
 
 int LevenshteinDistance(const string& A, const string& B) {
-  // TODO - you fill in here.
-  return 0;
+
+  vector<vector<int>> dp (A.size()+1, vector<int> (B.size()+1));
+
+  for (int i = 0; i <= A.size(); ++i)
+    dp[i][0] = i;
+  for (int i = 0; i <= B.size(); ++i)
+    dp[0][i] = i;
+
+  for (int i = 1; i <= A.size(); ++i) {
+    for (int j = 1; j <= B.size(); ++j) {
+      if (A[i-1] == B[j-1]) dp[i][j] = dp[i-1][j-1];
+      else
+        dp[i][j] = std::min(dp[i-1][j-1], std::min(dp[i-1][j], dp[i][j-1])) + 1;
+    }
+  }
+
+  return dp[A.size()][B.size()];
 }
 
 int main(int argc, char* argv[]) {
