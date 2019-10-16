330e0b18c6a727500e39cf5641e782a52ebeaf7d
Stephen Martin <lockwood@opperline.com>
2019-10-16 08:01:04 -0700

    6.12 find substring in string

--- epi_judge_cpp/substring_match.cc
+++ epi_judge_cpp/substring_match.cc
@@ -5,8 +5,19 @@ using std::string;
 // Returns the index of the first character of the substring if found, -1
 // otherwise.
 int RabinKarp(const string &t, const string &s) {
-  // TODO - you fill in here.
-  return 0;
+  if (s.empty()) return 0;
+  if (s.size() > t.size()) return -1;
+
+  for (int i = 0; i < t.size(); ++i) {
+    int j = 0, k = i;
+    while (j < s.size() && t[k] == s[j]) {
+      j++;
+      k++;
+    }
+    if (j == s.size()) return i;
+  }
+
+  return -1;
 }
 
 int main(int argc, char *argv[]) {
